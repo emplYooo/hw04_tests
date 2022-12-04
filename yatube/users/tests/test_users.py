254208574476a3a1d660380f1c5f0bd8dd712a17
Дмитрий Евстратов <imbazzz123@gmail.com>
2022-12-04 19:01:12 +0300

    sdad

--- yatube/users/tests/test_users.py
+++ yatube/users/tests/test_users.py
@@ -1,3 +1,5 @@
+import secrets
+
 from django.contrib.auth import get_user_model
 from django.test import Client, TestCase
 from django.urls import reverse
@@ -11,24 +13,22 @@ class UserCreateTests(TestCase):
     def setUp(self):
         self.user_client = Client()
 
-    def test_create_user_not_true(self):
-        """Проверка что создается"""
+    def test_create_user(self):
+        """Проверка что пользователь создается"""
         users_count = User.objects.count()
-        print('users' + str(users_count))
+        password = secrets.token_urlsafe()
         form_data = {
             'first_name': 'Имя',
             'last_name': 'Фамилия',
             'username': 'emplyooo',
             'email': 'emplyooo@gmail.com',
-            'password1': '12345678900987654321',
-            'password2': '12345678900987654321',
+            'password1': password,
+            'password2': password,
         }
         response = self.user_client.post(
             reverse('users:signup'),
             data=form_data,
             follow=True
         )
-        t = User.objects.count()
-        print('peremennaya', response.status_code, t)
-        self.assertEqual(User.objects.count(), users_count)
+        self.assertEqual(User.objects.count(), users_count + 1)
         self.assertEqual(response.status_code, 200)
